2416bd42e575441f33c659f98844d4edfa39d8bc
Script Raccoon <scriptraccoon@gmail.com>
2025-10-25 04:41:08 +0200

    show image

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,14 +5,43 @@
 	<a href="https://www.youtube.com/c/DerArtgenosse" target="_blank"
 		>Artgenossen
 	</a>
-	für eine Person zu finden, die lieber
-	<a href="/omni-bingo.webp">Omni-Bingo</a>
-	spielt statt sachlich zu diskutieren.
+	für eine Person zu finden, die lieber <i>Omni-Bingo</i> spielt anstatt sachlich
+	zu diskutieren.
 </p>
 
+<img
+	src="/omni-bingo.webp"
+	alt="Omni-Bingo:
+		1. Ich find's toll, dass du das machst.
+		2. Darauf erstmal ein Steak.
+		3. Veganer halten sich für was Besseres.
+		4. Löwen essen auch Fleisch.
+		5. Aber Fisch isst du noch oder?
+		6. Woher bekommst du dann Proteine?
+		7. Ihr Veganer seid so militant.
+		8. Vegane Produkte sind pure Chemie.
+		9. Menschen haben immer Fleisch gegessen.
+		10. Veganismus ist wie eine Religion.
+		11. Veganer sind intolerant gegenüber anderen.
+		12. Soja zerstört den Regenwald.
+		13. Ich esse jetzt zum Ausgleich doppelt so viel Fleisch.
+		14. Ohne Tiermast lebten viele Tiere gar nicht erst.
+		15. Veganer zwingen einem die Meinung auf.
+		16. Käse ist einfach zu lecker.
+		17. Wozu haben wir dann Eckzähne?
+		18. Ich könnte das nicht.
+		19. 100% vegan geht gar nicht.
+		20. Supplemente sind nicht natürlich.
+		21. Indigene Völker können nicht vegan leben.
+		22. Dank Fleisch hat der Mensch sich entwickelt.
+		23. Ein bisschen vermisst du es aber schon oder?
+		24. Ich kaufe nur beim Metzger meines Vertrauens."
+/>
+
 <p>
-	Benutze das Suchfeld und die passenden Videos werden sofort angezeigt. Die URL
-	kann unmittelbar kopiert werden.
+	Benutze das Suchfeld und die passenden Videos werden sofort angezeigt. Dazu
+	werden sowohl Titel als auch Beschreibung durchsucht. Die URL kann unmittelbar
+	kopiert werden.
 </p>
 
 <p>
@@ -29,12 +58,13 @@
 	> entwickelt.
 </p>
 
-<p>
-	<a href="/">Zurück zur Suche</a>
-</p>
-
 <style>
 	h2 {
 		margin-bottom: 1rem;
+	}
+
+	img {
+		clip-path: inset(24px 0 0 0);
+		margin-top: -24px;
 	}
 </style>
